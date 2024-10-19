97a695dd585e6ef9cfc710947e842dea501d2830
ftj <ftj072514@gmail.com>
2024-10-19 16:34:15 +0800

    cope with I/O

--- Project3/main.c
+++ Project3/main.c
@@ -11,48 +11,37 @@ struct squarenode {
     int direc[4];
 }typedef node;
 
+
+int backtrack(int** garden, int r, int c, int row, int col, node** connector);
 int main() {
-    int r,c;
+    int r,c, num = 0, capacity = 20;
     scanf("%d %d",&r,&c);
-    int garden[r][c];
-
-
+    int** garden = (int**)malloc(sizeof(int*) * r);
+    node* connector = (node*)malloc(sizeof(node) * capacity);
     for(int i=0;i<r;i++) //read garden 
     {
+        garden[i] = (int*)malloc(sizeof(int) * c);
         for(int j=0;j<c;j++)
         {
             scanf("%d",&garden[i][j]);
-        }
-    }
-
-    int num=0;  // connector number
-
-    for(int i=0;i<r;i++)
-    {
-        for(int j=0;j<c;j++)
-        {
-            if(garden[i][j] != 0)
-            {
-                num++;
+            if(garden[i][j] != 0){
+                if(num + 1 > capacity){
+                    capacity *= 2;
+                    connector = (node*)realloc(connector, capacity * sizeof(node));
+                }
+                connector[num].row = i;
+                connector[num].col = j;
+                connector[num++].deg = garden[i][j];
             }
         }
     }
-
-    node connector[num];
-
-    int t=0;
-    for(int i=0;i<r;i++)
-    {
-        for(int j=0;j<c;j++)
-        {
-            if(garden[i][j] != 0)
-            {
-                //read connnector
-                t++;
-            }
+    if(backtrack(garden, r, c, 0, 0, connector)){
+        for(int i = 0; i < num ; i++){
+            printf("%d %d %d %d %d %d\n", connector[i].row, connector[i].col, connector[i].direc[0], connector[i].direc[1], connector[i].direc[2], connector[i].direc[3]);
         }
     }
-
-
-    
+    else{
+        printf("No Solution");
+    }
+    return 0;
 }
